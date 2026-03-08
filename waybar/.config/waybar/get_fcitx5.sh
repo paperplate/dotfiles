14e31333f88d9c49290ca5189bee6eae90a2c2a5
paperplate <seanbergunder@outlook.com>
2026-03-08 15:19:20 -0400

    Fix chinese notification

--- waybar/.config/waybar/get_fcitx5.sh
+++ waybar/.config/waybar/get_fcitx5.sh
@@ -5,6 +5,6 @@ current=$(fcitx5-remote -n 2>/dev/null)
 
 case "$current" in
   "keyboard-us") echo "EN" ;;
-  "keyboard-pinyin") echo "中文" ;;
+  "pinyin") echo "中文" ;;
   *) echo "${current^^}" ;; # fallback: uppercase the name
 esac
